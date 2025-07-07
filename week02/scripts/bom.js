dad7947087aaaa902605cebfbc8466bfca233272
Adeola Tolulope Adetunji <adeolatolulope29@hotmail.com>
2025-07-07 17:13:03 +0100

    week02

---
+++ week02/scripts/bom.js
@@ -0,0 +1,29 @@
+
+const input = document.querySelector('#favchap');
+const button = document.querySelector('#addButton');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', function () {
+  const chapter = input.value.trim();
+
+  if (chapter !== '') {
+    const li = document.createElement('li');
+    const deleteButton = document.createElement('button');
+
+    li.textContent = chapter;
+    deleteButton.textContent = '❌';
+    deleteButton.setAttribute('aria-label', `Remove ${chapter}`);
+    li.appendChild(deleteButton);
+
+    list.appendChild(li);
+
+    deleteButton.addEventListener('click', function () {
+      list.removeChild(li);
+      input.focus(); 
+    });
+
+    input.value = '';
+  }
+
+  input.focus();
+});
